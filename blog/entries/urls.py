ad6f32c3d0b74ac872d10ee623db2446d944d9e3
vinothssv <Vinoth Sundararajan@tsi.com>
2020-04-30 22:11:15 +0530

    DJANGO: Django readme page redirect to new

--- blog/entries/urls.py
+++ blog/entries/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, EntryView
+
 urlpatterns = [
     #path('home/', HomeView.as_view(), name = 'blog-home')
-    path('', HomeView.as_view(), name = 'blog-home')
+    path('', HomeView.as_view(), name = 'blog-home'),
+    path ('entry/<int:pk>/', EntryView.as_view(), name = "entry-detail")
 ]
--- blog/entries/views.py
+++ blog/entries/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Entry
 # Create your views here.
 
@@ -7,3 +7,7 @@ class HomeView(ListView):
     model = Entry
     template_name = 'entries/index.html'
     context_object_name = "blog_entries"
+
+class EntryView(DetailView):
+    model = Entry
+    template_name = 'entries/entry_detail.html'
