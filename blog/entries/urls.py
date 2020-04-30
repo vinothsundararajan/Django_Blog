a3cb1f132202b000e61d56d5efe27ef955742939
vinothssv <Vinoth Sundararajan@tsi.com>
2020-04-30 23:52:48 +0530

    DJANGO: Django Method added to control page -42.00

--- blog/entries/urls.py
+++ blog/entries/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, EntryView
+from .views import HomeView, EntryView, CreateEntryView
 
 urlpatterns = [
     #path('home/', HomeView.as_view(), name = 'blog-home')
     path('', HomeView.as_view(), name = 'blog-home'),
-    path ('entry/<int:pk>/', EntryView.as_view(), name = "entry-detail")
+    path ('entry/<int:pk>/', EntryView.as_view(), name = "entry-detail"),
+    path ('create_entry/', CreateEntryView.as_view(success_url="/"), name = "create_entry")
 ]
--- blog/entries/views.py
+++ blog/entries/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Entry
 # Create your views here.
 
@@ -7,7 +7,18 @@ class HomeView(ListView):
     model = Entry
     template_name = 'entries/index.html'
     context_object_name = "blog_entries"
+    ordering = ['-entry_date']
+    paginate_by = 3
 
 class EntryView(DetailView):
     model = Entry
     template_name = 'entries/entry_detail.html'
+
+class CreateEntryView(CreateView):
+    model = Entry
+    template_name = 'entries/create_entry.html'
+    fields = [ 'entry_title', 'entry_text' ]
+
+    def form_valid(self,form):
+        form.instance.entry_author = self.request.user
+        return super().form_valid(form)
