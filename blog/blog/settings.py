1c27fbdea1f5b1dac9576fa0020b4642057f690e
vinothssv <Vinoth Sundararajan@tsi.com>
2020-05-01 11:35:04 +0530

    DJANGO: STATIC_ROOT Updated path

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -115,5 +115,5 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'entries/static'),
 )
